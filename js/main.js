8e0b2333166101401e440124a98c58a01107bf6b
La Nhat Thang <lanhatthang1910@gmail.com>
2024-01-31 18:10:55 +0700

    Add new project food

---
+++ js/main.js
@@ -0,0 +1,10 @@
+console.log("May la thang dbrr")
+
+window.addEventListener('load', function() {
+    var loadingScreen = document.getElementById('loading-screen');
+    var content = document.getElementById('content-web');
+
+    loadingScreen.style.display = 'none';
+    content.style.display = 'block';
+});
+
