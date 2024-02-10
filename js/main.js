57168745ea1b6bffe78caef0a45fa2f2e42575d1
La Nhat Thang <lanhatthang1910@gmail.com>
2024-02-10 15:36:34 +0700

    Test day code len cpanel

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-console.log("May la thang dbrr")
-
-window.addEventListener('load', function() {
+// Loading trang web
+window.addEventListener('load', function () {
     var loadingScreen = document.getElementById('loading-screen');
     var content = document.getElementById('content-web');
 
@@ -8,3 +7,16 @@ window.addEventListener('load', function() {
     content.style.display = 'block';
 });
 
+// Đổi theme
+function changeColor() {
+    var myDiv = document.getElementById('mydiv');
+    var currentColor = myDiv.getAttribute('data-bs-theme');
+
+    var light = 'light';
+    var dark = 'dark';
+    if (currentColor == light) {
+        myDiv.setAttribute('data-bs-theme', dark);
+    } else {
+        myDiv.setAttribute('data-bs-theme', light);
+    }
+}
